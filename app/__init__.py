8f34c810f091432a49da25445b884a10cef2b80f
nickyzj <reg.zj@outlook.com>
2016-02-14 18:47:21 +0800

    database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+track_modifications = app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', True)
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
