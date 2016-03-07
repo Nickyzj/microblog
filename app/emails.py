d097ba236557bdeff84ea4fb7a8e6bda97f42f1d
nickyzj <reg.zj@outlook.com>
2016-03-07 13:40:34 +0800

    email thread

--- app/emails.py
+++ app/emails.py
@@ -2,12 +2,19 @@ from flask.ext.mail import Message
 from app import mail
 from flask import render_template
 from config import ADMINS
+from .decorators import async
+
+@async
+def send_async_email(app, msg):
+	with app.app_context():
+		mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
 	msg = Message(subject, sender = ADMINS[0], recipients = recipients)
 	msg.body = text_body
 	msg.html = html_body
-	mail.send(msg)
+	# mail.send(msg)
+	send_async_email(app, msg)
 
 def follower_notification(followed, follower):
 	print("--> %s Sending email" % follower.nickname)
